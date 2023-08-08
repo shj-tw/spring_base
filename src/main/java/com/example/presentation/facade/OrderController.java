0b5b158cdad538546764a2e6ecfe40c7d238b9fd
yang.hu <yang.hu@thoughtworks.com>
2023-08-08 16:24:52 +0800

    [yang] add controller for order

---
+++ src/main/java/com/example/presentation/facade/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.presentation.facade;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+//    @PostMapping("/submit")
+//    public OrderDto submit(@RequestBody OrderRequest orderRequest,
+//                           @RequestParam(name = "customerId" String customerId)){
+//        return
+//    }
+
+}
